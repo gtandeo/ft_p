cd612e850f278a7cc3dbfe13a3276f7ae70c7c7d
gtandeo <tandeo.gurvan@gmail.com>
2016-10-04 05:29:43 +0200

    rendu

--- srcs/wait_server.c
+++ srcs/wait_server.c
@@ -10,24 +10,35 @@ static char	*get_file_name(char *command)
 	return (ft_strdup(command + i + 1));
 }
 
+static int	wait_server_pass(int sock)
+{
+	char	*line;
+
+	while (get_next_line(sock, &line) > 0 && ft_strcmp(line, "\033"))
+	{
+		if (!ft_strcmp(line, "get_ok"))
+		{
+			free(line);
+			return (0);
+		}
+		ft_putendl(line);
+		free(line);
+	}
+	return (1);
+}
+
 static void	wait_get(int sock, char *command)
 {
 	char	*line;
 	char	*file_name;
 	int		fd;
 
-	get_next_line(sock, &line);
-	if (ft_strcmp(line, "get_ok"))
-	{
-		ft_putendl(line);
-		free(line);
+	if (wait_server_pass(sock))
 		return ;
-	}
-	free(line);
 	file_name = get_file_name(command);
-	if ((fd = open(file_name, O_CREAT | O_WRONLY | O_APPEND)) == -1)
+	if ((fd = open(file_name, O_CREAT | O_WRONLY | O_APPEND, 755)) == -1)
 		return ;
-	while (get_next_line(sock, &line))
+	while (get_next_line(sock, &line) > 0 && ft_strcmp(line, "\033"))
 	{
 		ft_putendl_fd(line, fd);
 		free(line);
